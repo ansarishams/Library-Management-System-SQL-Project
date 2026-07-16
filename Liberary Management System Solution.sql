0c39607899b1b5ae2e9d8e5148d4aadf194de986
SHAMSUL HODA <shamsansari269@gmail.com>
2026-07-16 14:06:59 +0530

    Refactor SQL script for library management tasks

--- Liberary Management System Solution.sql
+++ Liberary Management System Solution.sql
@@ -1,29 +1,29 @@
 					-- PROJECT TASK
 
---  Task 1. Create a New Book Record -- "978-1-60129-456-2', 'To Kill a Mockingbird', 'Classic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.')"
+-- 1. Create a New Book Record -- "978-1-60129-456-2', 'To Kill a Mockingbird', 'Classic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.')"
 
 INSERT INTO books(isbn, book_title, category, rental_price, status, author, publisher)
 VALUES('978-1-60129-456-2', 'To Kill a Monkingbird', 'Classis', 6.00, 'Yes', 'Harper Lee', 'J.B. Lippincott & Co. ');
 
--- Task 2: Update an Existing Member's Address
+-- 2: Update an Existing Member's Address
 
 UPDATE members
 SET member_address = '125 Left New York'
 WHERE member_id = 'C101';
 
--- Task 3: Delete a Record from the Issued Status Table -- Objective: Delete the record with issued_id = 'IS121' from the issued_status table.
+-- 3: Delete a Record from the Issued Status Table -- Objective: Delete the record with issued_id = 'IS121' from the issued_status table.
 
 DELETE FROM issued_status
 WHERE issued_id = 'IS121';
 
--- Task 4: Retrieve All Books Issued by a Specific Employee -- Objective: Select all books issued by the employee with emp_id = 'E101'.
+-- 4: Retrieve All Books Issued by a Specific Employee -- Objective: Select all books issued by the employee with emp_id = 'E101'.
 
 SELECT 
 	* 
 FROM issued_status
 WHERE issued_emp_id = 'E101';
 
--- Task 5: List Members Who Have Issued More Than One Book -- Objective: Use GROUP BY to find members who have issued more than one book.
+-- 5: List Members Who Have Issued More Than One Book -- Objective: Use GROUP BY to find members who have issued more than one book.
 
 SELECT 
 	issued_emp_id,
@@ -32,7 +32,7 @@ FROM issued_status
 GROUP BY issued_emp_id
 HAVING COUNT(*)>1;
 
--- Task 6: Create Summary Tables: Used CTAS to generate new tables based on query results - each book and total book_issued_cnt**
+-- 6: Create Summary Tables: Used CTAS to generate new tables based on query results - each book and total book_issued_cnt**
 
 CREATE TABLE book_cnt AS
 SELECT 
@@ -44,14 +44,14 @@ JOIN issued_status i
 ON b.isbn = i.issued_book_isbn
 GROUP BY b.book_title, b.isbn;
 
--- Task 7. Retrieve All Books in a Specific Category:
+-- 7. Retrieve All Books in a Specific Category:
 
 SELECT 
 	*
 FROM books
 WHERE category = 'Classic';
 
--- Task 8: Find Total Rental Income by Category:
+-- 8: Find Total Rental Income by Category:
 
 SELECT 
 	b.category, 
@@ -62,14 +62,14 @@ JOIN issued_status i
 ON b.isbn = i.issued_book_isbn
 GROUP BY b.category;
 
--- Task 9: List Members Who Registered in the Last 180 Days:
+-- 9: List Members Who Registered in the Last 180 Days:
 
 SELECT 
 	* 
 FROM members
 WHERE reg_date >= CURRENT_DATE - INTERVAL '5 year';
 
--- Task 10: List Employees with Their Branch Manager's Name and their branch details:
+-- 10: List Employees with Their Branch Manager's Name and their branch details:
 
 SELECT 
 	e.emp_id, 
@@ -85,7 +85,7 @@ JOIN employees e1
 ON e1.emp_id = b.manager_id;
 
 
--- Task 11. Create a Table of Books with Rental Price Above a Certain Threshold:
+-- 11. Create a Table of Books with Rental Price Above a Certain Threshold:
 
 CREATE TABLE expensive_book AS
 SELECT 
@@ -93,7 +93,7 @@ SELECT
 FROM books
 WHERE rental_price>=7;
 
--- Task 12: Retrieve the List of Books Not Yet Returned
+-- 12: Retrieve the List of Books Not Yet Returned
 
 SELECT 
 	* 
@@ -103,7 +103,7 @@ ON r.issued_id = i.issued_id
 WHERE r.return_id IS NULL;
 
 
--- Task 13: Identify Members with Overdue Books
+-- 13: Identify Members with Overdue Books
 -- Write a query to identify members who have overdue books (assume a 30-day return period). Display the member's_id, member's name, book title, issue date, and days overdue.
 
 SELECT 
